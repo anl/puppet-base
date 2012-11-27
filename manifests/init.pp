aa6fbff7259b484dcb1153a1b1fe3f8d52b8854c
Andrew Leonard <andy@hurricane-ridge.com>
2012-11-26 20:30:14 -0800

    Add common packages.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 # == Class: base
 #
-# Full description of class base here.
+# A Puppet module intended to be applied to all hosts.  Generally, this means
+# simple, one-off actions, such as ungrouped package installations.
 #
 # === Parameters
 #
@@ -10,32 +11,22 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #   e.g. "Specify one or more upstream ntp servers as an array."
 #
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
-#
 # === Examples
 #
-#  class { base:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
-#  }
+#  include base
 #
 # === Authors
 #
-# Author Name <author@domain.com>
+# Andrew Leonard
 #
 # === Copyright
 #
-# Copyright 2011 Your name here, unless otherwise noted.
+# Copyright 2012 Andrew Leonard
 #
 class base {
 
+  $pkgs = [ 'git', 'tcpdump', 'traceroute' ]
+
+  package { $pkgs: ensure => present }
 
 }
